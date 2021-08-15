b60bc45f1dc3ab1a3f705a615d6c852ff3b6a593
Samuel Song <samsongs@umich.edu>
2021-08-15 14:01:18 -0600

    Add validators

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -16,6 +17,9 @@ app.get("/", (req, res) => res.send("Hello World!!!"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
