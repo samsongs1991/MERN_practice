1ff2d94ebd9b54fae54a980e676bba16eb71e31c
Samuel Song <samsongs@umich.edu>
2021-08-14 18:24:38 -0600

    Import bodyParser and add middleware

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
 
 app.get("/", (req, res) => res.send("Hello World!!!"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
