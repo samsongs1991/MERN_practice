c718e9d126d6f6f0b32da4ed4d2bfea25a365d70
Samuel Song <samsongs@umich.edu>
2021-08-14 19:07:10 -0600

    Add a script to display message on the chrome console

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!!!"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
